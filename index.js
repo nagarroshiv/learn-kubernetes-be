5bbadb1bcaeb5f0210278e9aaa4ca1a3b3fb3cb6
Shiv Sharma <shiv.sharma@nagarro.com>
2023-04-25 13:24:14 +0530

    get and post

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const cors = require("cors");
+const mongoose = require("mongoose")
+const pino = require('pino-http')()
+const { Post } = require("./posts.model");
+
+const app = express()
+app.use(pino)
+app.use(cors())
+app.use(express.json());
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send(`Node API v1 is listening on ${port}`)
+})
+
+app.get(`/api/posts`, async (request, response) => {
+    const allPosts = await Post.find();
+    return response.status(200).send({ data: allPosts })
+})
+
+app.post(`/api/post`, async (req, res) => {
+    const newPost = new Post({ ...req.body });
+    const insertedPost = await newPost.save();
+    return res.status(201).json(insertedPost);
+})
+
+
+const start = async () => {
+    try {
+        await mongoose.connect(
+            "mongodb+srv://root:ABC123@cluster0.021zaql.mongodb.net/test"
+        );
+        app.listen(port, () => {
+            console.log(`Node API is listening on ${port}`)
+        })
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+start();
